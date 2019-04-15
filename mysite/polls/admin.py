e5f694fc2fd581527315a291544ba54fbbac70da
solheng <solheng@foxmail.com>
2019-04-15 20:49:21 +0800

    new_branch

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -6,11 +6,13 @@ class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
